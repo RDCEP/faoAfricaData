16b96f569cc2f153b2c37d18eb1aa8c39433c9b2
Neil Best <nbest@ci.uchicago.edu>
2013-12-04 16:54:05 -0600

    deleted Org output from tangled gadmNames.R

--- gadmNames.R
+++ gadmNames.R
@@ -46,28 +46,3 @@ write.csv( gadmNames[ gadmNames$ISO == "MWI",
     file= "gadmNamesMWI.csv",
     row.names= FALSE,
     na= "")
-
-| iso | fn                                                           | adm |
-|-----+--------------------------------------------------------------+-----|
-| AGO | Shapefiles/R Data/Angola/AGO_adm1.RData                      |   1 |
-| BDI | Shapefiles/R Data/Burundi/BDI_adm1.RData                     |   1 |
-| BEN | Shapefiles/R Data/Benin/BEN_adm1.RData                       |   1 |
-| BFA | Shapefiles/R Data/Burkina Faso/BFA_adm1.RData                |   1 |
-| BFA | Shapefiles/R Data/Burkina Faso/BFA_adm2.RData                |   2 |
-| CIV | Shapefiles/R Data/Côte d'Ivoire/CIV_adm1.RData               |   1 |
-| CMR | Shapefiles/R Data/Cameroon/CMR_adm1.RData                    |   1 |
-| ETH | Shapefiles/R Data/Ethiopia/ETH_adm1.RData                    |   1 |
-| GHA | Shapefiles/R Data/Ghana/GHA_adm1.RData                       |   1 |
-| GMB | Shapefiles/R Data/Gambia/GMB_adm1.RData                      |   1 |
-| GNB | Shapefiles/R Data/Guinea-Bissau/GNB_adm1.RData               |   1 |
-| KEN | Shapefiles/R Data/Kenya/KEN_adm1.RData                       |   1 |
-| MLI | Shapefiles/R Data/Mali/MLI_adm1.RData                        |   1 |
-| MOZ | Shapefiles/R Data/Mozambique/MOZ_adm1.RData                  |   1 |
-| MWI | Shapefiles/R Data/Malawi/MWI_adm1.RData                      |   1 |
-| NER | Shapefiles/R Data/Niger/NER_adm1.RData                       |   1 |
-| NGA | Shapefiles/R Data/Nigeria/NGA_adm1.RData                     |   1 |
-| RWA | Shapefiles/R Data/Rwanda/RWA_adm1.RData                      |   1 |
-| RWA | Shapefiles/R Data/Rwanda/RWA_adm2.RData                      |   2 |
-| TGO | Shapefiles/R Data/Togo/TGO_adm1.RData                        |   1 |
-| TZA | Shapefiles/R Data/United Republic of Tanzania/TZA_adm1.RData |   1 |
-| ZMB | Shapefiles/R Data/Zambia/ZMB_adm1.RData                      |   1 |
